bff3528e07f16234a8166360f300e117ab2ebae5
snivalkar98 <snivalkar1998@gmail.com>
2025-11-28 19:06:14 +0530

    Express js dynamic content

---
+++ expressjs/src/index.js
@@ -0,0 +1,32 @@
+const path      = require("path");
+const express   = require('express');
+const app       = express();
+
+const staticPath = path.join(__dirname,'../public');
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.join(__dirname,'../public'));
+
+// to set view engine
+app.set("view engine","hbs");
+
+//builtin middleware
+// app.use(express.static(staticPath));
+
+app.get("", (req,res) => {
+    res.render('index',{
+        channelname:"SSN",
+    });
+});
+
+app.get("/", (req,res) => {
+    res.send("hello from SSN");
+});
+
+app.get("/about", (req,res) => {
+    res.send("hello from about");
+});
+
+app.listen(8000,() =>{
+    console.log("Listening the port at 8000");
+});
